e60629f7017df58570a9d8a0ff84a44ad3ac4d0e
Thomas Clausen <kontakt@thomasclausen.dk>
2012-03-01 15:56:43 +0100

    Correct localization

--- nord-custom-login-form.php
+++ nord-custom-login-form.php
@@ -17,7 +17,7 @@ function nord_password_form() {
 	$output .= '<p><strong>' . __( 'Denne side er kodeordsbeskyttet!', 'nord_password_form' ) . '</strong></p>' . "\r\n";
 	$output .= '<p><em>' . __( 'Leder du efter adresse til login i forbindelse med tilmelding af hold skal du &aring;bne f&oslash;lgende side - <a href="https://www.conventus.dk/medlemslogin/index.php?forening=944" target="_blank">https://www.conventus.dk/medlemslogin/index.php?forening=944</a></em></p>', 'nord_password_form' ) . "\r\n";
 	$output .= '<p>' . __( 'For at se siden skal du indtaste dit kodeord nedenunder.', 'nord_password_form' ) . '<br /><em>' . __( 'Bem&aelig;rk! Der er forskel p&aring; store og sm&aring; bogstaver i kodeordet.', 'nord_password_form' ) . '</em></p>' . "\r\n";
-	$output .= '<p><label for="' . $label . '">' . __( 'Password' ) . ':<br /><input name="post_password" id="' . $label . '" type="password" size="20" /></label><br /><input type="submit" name="Submit" value="' . esc_attr__( 'Submit' ) . '" /></p>' . "\r\n";
+	$output .= '<p><label for="' . $label . '">' . __( 'Password', 'nord_password_form' ) . ':<br /><input name="post_password" id="' . $label . '" type="password" size="20" /></label><br /><input type="submit" name="Submit" value="' . esc_attr__( 'Submit', 'nord_password_form' ) . '" /></p>' . "\r\n";
 	$output .= '</form>' . "\r\n";
 	
 	return $output;
